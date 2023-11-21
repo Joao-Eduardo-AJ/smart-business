ba9783b759cb485beb5cb197e7d24223fdcc773a
Joao-Eduardo-AJ <joaoeduaj@gmail.com>
2023-11-20 23:48:51 -0300

    feat(src/components): adds scroll event to metrics component

--- src/components/metrics/index.tsx
+++ src/components/metrics/index.tsx
@@ -1,11 +1,32 @@
+import { useInView } from "react-intersection-observer";
+
 interface IMetrics {
   number: string;
   legend: string;
 }
 
-export const Metrics = ({ number, legend }: IMetrics) => (
-  <div className="w-max flex flex-col gap-2">
-    <p className="text-lg1 lg:text-lg2">{number}</p>
-    <p className="text-md">{legend}</p>
-  </div>
-);
+export const Metrics = ({ number, legend }: IMetrics) => {
+  const { ref, inView } = useInView({
+    threshold: 1,
+    delay: 350,
+  });
+
+  return (
+    <div ref={ref} className="w-160 h-110 relative overflow-hidden">
+      <p
+        className={`transition-all duration-500 text-lg1 lg:text-lg2 absolute top-0 ${
+          inView ? "left-0 opacity-100" : "-left-60 opacity-0"
+        }`}
+      >
+        {number}
+      </p>
+      <p
+        className={`transition-all duration-500 w-160 text-md absolute bottom-0 ${
+          inView ? "left-0 opacity-100" : "left-100 opacity-0"
+        }`}
+      >
+        {legend}
+      </p>
+    </div>
+  );
+};
